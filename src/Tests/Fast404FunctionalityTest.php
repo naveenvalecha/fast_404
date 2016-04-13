4a9cb6375e92d637a88555925169fcf57b8bb249
Naveen Valecha <er.naveenvalecha@gmail.com>
2016-04-13 11:28:01 +0530

    Updated Tests.

--- src/Tests/Fast404FunctionalityTest.php
+++ src/Tests/Fast404FunctionalityTest.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\action\Tests\BulkFormTest.
- */
-
 namespace Drupal\fast404\Tests;
 
 use Drupal\Core\Site\Settings;
@@ -15,20 +10,23 @@ use Drupal\simpletest\WebTestBase;
  *
  * @group fast404
  */
-class UrlExtensionCheckTest extends WebTestBase {
+class Fast404FunctionalityTest extends WebTestBase {
 
   /**
    * Modules to install.
    *
    * @var array
    */
-  public static $modules = array('fast404');
+  public static $modules = ['system_test', 'fast404'];
 
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() {
+  protected $adminUser;
+
+  protected function setUp() {
     parent::setUp();
+    // Create an administrative user.
+    $this->adminUser = $this->drupalCreateUser(['administer site configuration']);
+    $this->adminUser->roles[] = 'administrator';
+    $this->adminUser->save();
   }
 
   /**
@@ -36,16 +34,18 @@ class UrlExtensionCheckTest extends WebTestBase {
    */
   public function testUrlCheck() {
     // Ensure path check isn't activated by default.
-    $this->drupalGet('notdrupal');
+    $this->drupalGet($this->randomMachineName(10));
     $this->assertResponse(404);
     $this->assertText('The requested page could not be found.');
 
+    $this->drupalLogin($this->adminUser);
     $settings = Settings::getAll();
     $settings['fast404_path_check'] = TRUE;
     $settings['fast404_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
     new Settings($settings + Settings::getAll());
+    $this->drupalLogout();
 
-    $this->drupalGet('notdrupal');
+    $this->drupalGet($this->randomMachineName(10));
     $this->assertResponse(404);
     $this->assertRaw('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "/notdrupal" was not found on this server.</p></body></html>');
 
